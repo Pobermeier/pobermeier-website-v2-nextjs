eb5285eef6e4d5edf97b34b8a53a958bc7a73fe7
Patrick Obermeier <patrick.obermeier@outlook.com>
2021-02-20 17:56:41 +0100

    added seo-component to 404-page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,10 @@
-const NotFound: React.FC = () => <h1>404 - Page Not Found</h1>;
+import SEO from '@/components/SEO/SEO';
+
+const NotFound: React.FC = () => (
+  <>
+    <SEO title="404 Not Found" />
+    <h1>404 - Not found</h1>
+  </>
+);
 
 export default NotFound;
