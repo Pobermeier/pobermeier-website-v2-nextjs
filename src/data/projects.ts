21c4054aa707bd81e38528d1e204f80054c11512
Patrick Obermeier <patrick.obermeier@outlook.com>
2021-02-20 12:56:41 +0100

    added projects data

---
+++ src/data/projects.ts
@@ -0,0 +1,357 @@
+import { Project } from 'models/project.model';
+
+const projects: Project[] = [
+  {
+    id: 'X-inoxAAACMAuWju',
+    uid: null,
+    type: 'project',
+    href:
+      'https://patrickobermeier.cdn.prismic.io/api/v2/documents/search?ref=X-iuGBAAACAAuYV-&q=%5B%5B%3Ad+%3D+at%28document.id%2C+%22X-inoxAAACMAuWju%22%29+%5D%5D',
+    tags: [],
+    first_publication_date: '2020-12-27T15:26:31+0000',
+    last_publication_date: '2020-12-27T15:26:31+0000',
+    slugs: ['battleship'],
+    linked_documents: [],
+    lang: 'en-de',
+    alternate_languages: [],
+    data: {
+      project_showcase_image: {
+        dimensions: { width: 1280, height: 960 },
+        alt: 'Battleship!',
+        copyright: null,
+        url:
+          'https://images.prismic.io/patrickobermeier/0a73ace1-0910-4935-be1f-7cbe33a36750_project_personal_battleship.jpg?auto=compress,format&rect=0,0,1280,960&w=1280&h=960',
+      },
+      project_logo: {
+        dimensions: { width: 48, height: 48 },
+        alt: 'Patrick Obermeier',
+        copyright: null,
+        url:
+          'https://images.prismic.io/patrickobermeier/27736f94-b716-416f-899c-5be952062be3_android-icon-48x48.png?auto=compress,format&rect=0,0,48,48&w=48&h=48',
+      },
+      project_title: [{ type: 'heading3', text: 'Battleship!', spans: [] }],
+      project_subtitle: [{ type: 'heading4', text: 'Multiplayer Game', spans: [] }],
+      project_type: 'Personal',
+      project_description:
+        'A multiplayer variant of the famous game "Battleship" implemented with Node.js, Express & Socket.io. Notable features are a game lobby to create & join games, in-game chat & 2-player multiplayer.',
+      technologies_used: 'html, css, js, node',
+      live_project_link: {
+        link_type: 'Web',
+        url: 'https://battleship-websockets-game.herokuapp.com/',
+      },
+      code_link: {
+        link_type: 'Web',
+        url: 'https://github.com/Pobermeier/battleship-websockets-game',
+      },
+    },
+  },
+  {
+    id: 'X-iuFRAAACIAuYVv',
+    uid: null,
+    type: 'project',
+    href:
+      'https://patrickobermeier.cdn.prismic.io/api/v2/documents/search?ref=X-iuGBAAACAAuYV-&q=%5B%5B%3Ad+%3D+at%28document.id%2C+%22X-iuFRAAACIAuYVv%22%29+%5D%5D',
+    tags: [],
+    first_publication_date: '2020-12-27T15:54:00+0000',
+    last_publication_date: '2020-12-27T15:54:00+0000',
+    slugs: ['hamsterrat'],
+    linked_documents: [],
+    lang: 'en-de',
+    alternate_languages: [],
+    data: {
+      project_showcase_image: {
+        dimensions: { width: 1280, height: 960 },
+        alt: 'HamsterRat',
+        copyright: null,
+        url:
+          'https://images.prismic.io/patrickobermeier/a84f741f-3ea3-4649-90f7-527988a62240_project_work_hamsterrat.jpg?auto=compress,format&rect=0,0,1280,960&w=1280&h=960',
+      },
+      project_logo: {
+        dimensions: { width: 48, height: 48 },
+        alt: 'Hamsterrat',
+        copyright: null,
+        url:
+          'https://images.prismic.io/patrickobermeier/72a76758-aadf-4d7d-a9bc-cb8f5e579337_HamsterRat_Icon_96x96.jpg?auto=compress,format&rect=0,0,96,96&w=48&h=48',
+      },
+      project_title: [{ type: 'heading3', text: 'HamsterRat', spans: [] }],
+      project_subtitle: [{ type: 'heading4', text: 'Business Website', spans: [] }],
+      project_type: 'Work',
+      project_description:
+        'I created this brochure website for my father who is running a consulting business. The main purpose of this site is to serve as a digital business card and FPOC for any potential clients. Optimizing keywords & other aspects related to SEO and optimizing loading speed were the most important considerations during the creation of this site.',
+      technologies_used: 'html, css, js, sass',
+      live_project_link: { link_type: 'Web', url: 'https://hamsterrat.com/' },
+      code_link: { link_type: 'Any' },
+    },
+  },
+  {
+    id: 'X-itWhAAACIAuYIp',
+    uid: null,
+    type: 'project',
+    href:
+      'https://patrickobermeier.cdn.prismic.io/api/v2/documents/search?ref=X-iuGBAAACAAuYV-&q=%5B%5B%3Ad+%3D+at%28document.id%2C+%22X-itWhAAACIAuYIp%22%29+%5D%5D',
+    tags: [],
+    first_publication_date: '2020-12-27T15:50:57+0000',
+    last_publication_date: '2020-12-27T15:50:57+0000',
+    slugs: ['social-casino-website'],
+    linked_documents: [],
+    lang: 'en-de',
+    alternate_languages: [],
+    data: {
+      project_showcase_image: {
+        dimensions: { width: 1280, height: 960 },
+        alt: 'Social Casino Website',
+        copyright: null,
+        url:
+          'https://images.prismic.io/patrickobermeier/1e49db2e-9f47-447c-a87b-ad45b94c88c7_project_work_lpgametwist.jpg?auto=compress,format&rect=0,0,1280,960&w=1280&h=960',
+      },
+      project_logo: {
+        dimensions: { width: 48, height: 48 },
+        alt: 'Greentube IES GmbH',
+        copyright: null,
+        url:
+          'https://prismic-io.s3.amazonaws.com/patrickobermeier/c4fc50f4-cfb6-41b9-be32-88671e4ea93d_Greentube_Icon_96x96.svg',
+      },
+      project_title: [{ type: 'heading3', text: 'Social Casino Website', spans: [] }],
+      project_subtitle: [{ type: 'heading4', text: 'Marketing Website', spans: [] }],
+      project_type: 'Work',
+      project_description:
+        'An example of a landing page for our reknown social casino brand. A PHP template engine and a headless CMS in the backend enable us to avoid code duplication and make marketing content editable by anybody. Combined with a focus on best-in-class loading speed optimizations this gives us a cutting edge over our competitors.',
+      technologies_used: 'html, css, js, php, sass, bootstrap, gulp',
+      live_project_link: { link_type: 'Web', url: 'https://lp.gametwist.com/landingpages/' },
+      code_link: { link_type: 'Any' },
+    },
+  },
+  {
+    id: 'X-irzxAAACEAuXtV',
+    uid: null,
+    type: 'project',
+    href:
+      'https://patrickobermeier.cdn.prismic.io/api/v2/documents/search?ref=X-iuGBAAACAAuYV-&q=%5B%5B%3Ad+%3D+at%28document.id%2C+%22X-irzxAAACEAuXtV%22%29+%5D%5D',
+    tags: [],
+    first_publication_date: '2020-12-27T15:44:18+0000',
+    last_publication_date: '2020-12-27T15:48:18+0000',
+    slugs: ['would-you-rather...'],
+    linked_documents: [],
+    lang: 'en-de',
+    alternate_languages: [],
+    data: {
+      project_showcase_image: {
+        dimensions: { width: 1280, height: 960 },
+        alt: 'Would you rather...?',
+        copyright: null,
+        url:
+          'https://images.prismic.io/patrickobermeier/11f1e893-a1a3-42f0-ae5c-d9eeb09ed058_project_personal_wouldyourather.jpg?auto=compress,format&rect=0,0,1280,960&w=1280&h=960',
+      },
+      project_logo: {
+        dimensions: { width: 48, height: 48 },
+        alt: 'Patrick Obermeier',
+        copyright: null,
+        url:
+          'https://images.prismic.io/patrickobermeier/27736f94-b716-416f-899c-5be952062be3_android-icon-48x48.png?auto=compress,format&rect=0,0,48,48&w=48&h=48',
+      },
+      project_title: [{ type: 'heading3', text: 'Would you rather...?', spans: [] }],
+      project_subtitle: [{ type: 'heading4', text: 'Quiz App', spans: [] }],
+      project_type: 'Personal',
+      project_description:
+        'An implementation of the popular "Would you rather..."-game that uses React for the UI-part and Redux for state management. The "backend" is implemented as an in-client DB + fake API endpoints.',
+      technologies_used: 'html, css, js, react, redux, bootstrap, webpack',
+      live_project_link: { link_type: 'Web', url: 'https://would-you-rather-2020.netlify.app/' },
+      code_link: { link_type: 'Web', url: 'https://github.com/Pobermeier/would-you-rather' },
+    },
+  },
+  {
+    id: 'X-itbBAAACEAuYJ-',
+    uid: null,
+    type: 'project',
+    href:
+      'https://patrickobermeier.cdn.prismic.io/api/v2/documents/search?ref=X-iuGBAAACAAuYV-&q=%5B%5B%3Ad+%3D+at%28document.id%2C+%22X-itbBAAACEAuYJ-%22%29+%5D%5D',
+    tags: [],
+    first_publication_date: '2020-12-27T15:52:11+0000',
+    last_publication_date: '2020-12-27T15:52:11+0000',
+    slugs: ['cash-casino-website', 'social-casino-website'],
+    linked_documents: [],
+    lang: 'en-de',
+    alternate_languages: [],
+    data: {
+      project_showcase_image: {
+        dimensions: { width: 1280, height: 960 },
+        alt: ' Cash Casino Website',
+        copyright: null,
+        url:
+          'https://images.prismic.io/patrickobermeier/84a6ad3d-eb76-40c5-8b1b-7ce0ad0db081_project_work_lpstargames.jpg?auto=compress,format&rect=0,0,1280,960&w=1280&h=960',
+      },
+      project_logo: {
+        dimensions: { width: 48, height: 48 },
+        alt: 'Greentube IES GmbH',
+        copyright: null,
+        url:
+          'https://prismic-io.s3.amazonaws.com/patrickobermeier/c4fc50f4-cfb6-41b9-be32-88671e4ea93d_Greentube_Icon_96x96.svg',
+      },
+      project_title: [{ type: 'heading3', text: 'Cash Casino Website', spans: [] }],
+      project_subtitle: [{ type: 'heading4', text: 'Mobile-App Landingpage', spans: [] }],
+      project_type: 'Work',
+      project_description:
+        'Due to the restrictive Google Play store policies regarding gambling apps we had to create separate landing pages, where users can download an APK-file instead, for all of our casino platforms. The most important aspect of this site is explaining the download- and installation instructions in a simple and concise manner.',
+      technologies_used: 'html, css, js, sass, bootstrap, grunt',
+      live_project_link: { link_type: 'Web', url: 'https://lp.stargames.de/mobile-app/en/' },
+      code_link: { link_type: 'Any' },
+    },
+  },
+  {
+    id: 'X94yHxAAACIAiv9z',
+    uid: null,
+    type: 'project',
+    href:
+      'https://patrickobermeier.cdn.prismic.io/api/v2/documents/search?ref=X-iuGBAAACAAuYV-&q=%5B%5B%3Ad+%3D+at%28document.id%2C+%22X94yHxAAACIAiv9z%22%29+%5D%5D',
+    tags: [],
+    first_publication_date: '2020-12-19T17:02:33+0000',
+    last_publication_date: '2020-12-19T17:10:38+0000',
+    slugs: ['vintage-poker'],
+    linked_documents: [],
+    lang: 'en-de',
+    alternate_languages: [],
+    data: {
+      project_showcase_image: {
+        dimensions: { width: 1280, height: 960 },
+        alt: 'Vintage Poke',
+        copyright: null,
+        url:
+          'https://images.prismic.io/patrickobermeier/0dbcd846-d2c4-46fc-8f4c-5129425d6274_project_personal_vintagepoker.jpg?auto=compress,format&rect=0,0,1280,960&w=1280&h=960',
+      },
+      project_logo: {
+        dimensions: { width: 48, height: 48 },
+        alt: 'Patrick Obermeier',
+        copyright: null,
+        url:
+          'https://images.prismic.io/patrickobermeier/27736f94-b716-416f-899c-5be952062be3_android-icon-48x48.png?auto=compress,format&rect=0,0,48,48&w=48&h=48',
+      },
+      project_title: [{ type: 'heading3', text: 'Vintage Poker', spans: [] }],
+      project_subtitle: [{ type: 'heading4', text: 'Poker Platform', spans: [] }],
+      project_type: 'Personal',
+      project_description:
+        'Vintage Poker is a poker platform built with the MERN-stack and Socket.io. The "Styled Components"-library was used to build a UI with a custom look & feel. Localized content (DE & EN) is being managed & loaded from the Headless-CMS Contentful.',
+      technologies_used: 'html, css, js, react, node, webpack',
+      live_project_link: { link_type: 'Web', url: 'https://www.vintagepoker.net/' },
+      code_link: { link_type: 'Web', url: 'https://github.com/Pobermeier/vintage-poker' },
+    },
+  },
+  {
+    id: 'X-iqoxAAACAAuXYo',
+    uid: null,
+    type: 'project',
+    href:
+      'https://patrickobermeier.cdn.prismic.io/api/v2/documents/search?ref=X-iuGBAAACAAuYV-&q=%5B%5B%3Ad+%3D+at%28document.id%2C+%22X-iqoxAAACAAuXYo%22%29+%5D%5D',
+    tags: [],
+    first_publication_date: '2020-12-27T15:39:20+0000',
+    last_publication_date: '2020-12-27T15:39:20+0000',
+    slugs: ['flashcards'],
+    linked_documents: [],
+    lang: 'en-de',
+    alternate_languages: [],
+    data: {
+      project_showcase_image: {
+        dimensions: { width: 1280, height: 960 },
+        alt: ' Flashcards',
+        copyright: null,
+        url:
+          'https://images.prismic.io/patrickobermeier/4ff6b0d2-0ea5-48fe-9923-bb85ab47ecd0_project_personal_flashcards.jpg?auto=compress,format&rect=0,0,1280,960&w=1280&h=960',
+      },
+      project_logo: {
+        dimensions: { width: 48, height: 48 },
+        alt: 'Patrick Obermeier',
+        copyright: null,
+        url:
+          'https://images.prismic.io/patrickobermeier/27736f94-b716-416f-899c-5be952062be3_android-icon-48x48.png?auto=compress,format&rect=0,0,48,48&w=48&h=48',
+      },
+      project_title: [{ type: 'heading3', text: 'Flashcards', spans: [] }],
+      project_subtitle: [{ type: 'heading4', text: 'Productivity App', spans: [] }],
+      project_type: 'Personal',
+      project_description:
+        'This is a productivity app created using React Native. Users can create virtual decks of flashcards, build a library of questions and answers for a specific topic and challenge themselves by taking quizzes and documenting their learning progress.',
+      technologies_used: 'js, css, react native',
+      live_project_link: {
+        link_type: 'Web',
+        url: 'https://snack.expo.io/@git/github.com/Pobermeier/reactnd-mobile-flashcards',
+      },
+      code_link: { link_type: 'Web', url: 'https://github.com/Pobermeier/mobile-flashcards' },
+    },
+  },
+  {
+    id: 'X-imiBAAACIAuWQM',
+    uid: null,
+    type: 'project',
+    href:
+      'https://patrickobermeier.cdn.prismic.io/api/v2/documents/search?ref=X-iuGBAAACAAuYV-&q=%5B%5B%3Ad+%3D+at%28document.id%2C+%22X-imiBAAACIAuWQM%22%29+%5D%5D',
+    tags: [],
+    first_publication_date: '2020-12-27T15:21:47+0000',
+    last_publication_date: '2020-12-27T15:21:47+0000',
+    slugs: ['simplysticky'],
+    linked_documents: [],
+    lang: 'en-de',
+    alternate_languages: [],
+    data: {
+      project_showcase_image: {
+        dimensions: { width: 1280, height: 960 },
+        alt: ' SimplySticky',
+        copyright: null,
+        url:
+          'https://images.prismic.io/patrickobermeier/bfd08ff2-f771-418c-b78d-01883d78d271_simplysticky.png?auto=compress,format&rect=0,0,1279,959&w=1280&h=960',
+      },
+      project_logo: {
+        dimensions: { width: 48, height: 48 },
+        alt: 'Patrick Obermeier',
+        copyright: null,
+        url:
+          'https://images.prismic.io/patrickobermeier/27736f94-b716-416f-899c-5be952062be3_android-icon-48x48.png?auto=compress,format&rect=0,0,48,48&w=48&h=48',
+      },
+      project_title: [{ type: 'heading3', text: 'SimplySticky', spans: [] }],
+      project_subtitle: [{ type: 'heading4', text: 'Note-taking App', spans: [] }],
+      project_type: 'Personal',
+      project_description:
+        'SimplySticky is a full-CRUD Note-taking App build with React which is utilizing cloud functions to perform any of its backend functionality. Uses Netlify Identity as an external auth provider & Redux for internal state management.',
+      technologies_used: 'html, css, js, react, redux, node, webpack',
+      live_project_link: { link_type: 'Web', url: 'https://www.simplysticky.net/' },
+      code_link: { link_type: 'Web', url: 'https://github.com/Pobermeier/simply-sticky' },
+    },
+  },
+  {
+    id: 'X-ilfBAAACIAuV9u',
+    uid: null,
+    type: 'project',
+    href:
+      'https://patrickobermeier.cdn.prismic.io/api/v2/documents/search?ref=X-iuGBAAACAAuYV-&q=%5B%5B%3Ad+%3D+at%28document.id%2C+%22X-ilfBAAACIAuV9u%22%29+%5D%5D',
+    tags: [],
+    first_publication_date: '2020-12-27T15:17:26+0000',
+    last_publication_date: '2020-12-27T15:17:26+0000',
+    slugs: ['devmeetup'],
+    linked_documents: [],
+    lang: 'en-de',
+    alternate_languages: [],
+    data: {
+      project_showcase_image: {
+        dimensions: { width: 1280, height: 960 },
+        alt: 'DevMeetup',
+        copyright: null,
+        url:
+          'https://images.prismic.io/patrickobermeier/8a528446-684b-44e8-ab64-9d78f0803eee_project_personal_devmeetup.jpg?auto=compress,format&rect=0,0,1280,960&w=1280&h=960',
+      },
+      project_logo: {
+        dimensions: { width: 48, height: 48 },
+        alt: 'Patrick Obermeier',
+        copyright: null,
+        url:
+          'https://images.prismic.io/patrickobermeier/27736f94-b716-416f-899c-5be952062be3_android-icon-48x48.png?auto=compress,format&rect=0,0,48,48&w=48&h=48',
+      },
+      project_title: [{ type: 'heading3', text: 'DevMeetup', spans: [] }],
+      project_subtitle: [{ type: 'heading4', text: 'Social Network', spans: [] }],
+      project_type: 'Personal',
+      project_description:
+        'A social network for developers built with the MERN-stack. Backend-API is set-up with Node.js and Express including authentication using JWT web tokens. The app uses Redux for client-side state management.',
+      technologies_used: 'html, css, js, react, redux, node, webpack',
+      live_project_link: { link_type: 'Web', url: 'https://www.devmeetup.net/' },
+      code_link: { link_type: 'Web', url: 'https://github.com/Pobermeier/devmeetup' },
+    },
+  },
+];
+
+export default projects;
