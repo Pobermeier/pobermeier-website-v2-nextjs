63b4d7bb03a32f7ebf1a19bf9a3f1f5bf1164c86
Patrick Obermeier <patrick.obermeier@outlook.com>
2021-02-20 17:56:22 +0100

    created global config-file

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+export default {
+  meta: {
+    defaultTitle: 'Patrick Obermeier | Frontend Developer & Online-Marketing Expert',
+    defaultDescription:
+      'Patrick Obermeier is an experienced front-end developer and online-marketing expert from Vienna, Austria. Creating web apps and websites using latest technologies like React & the MERN-Stack with passion!',
+    defaultKeywords:
+      'frontend developer, front-end developer, web developer, webdesign, web designer, html5, css, javascript, react, redux, mern-stack, node, mongodb, express, docker, linux, portfolio, projects, coder, programmer, web apps, websites, marketing sites, landing pages',
+    themeColor: '#f5f5f5',
+  },
+};
