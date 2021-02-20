fe21bf4c3442c4899f071f363ece53329bdd129f
Patrick Obermeier <patrick.obermeier@outlook.com>
2021-02-20 17:57:42 +0100

    wrap app in main layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { ComponentClass } from 'react';
 import { AppProps } from 'next/app';
 import 'styles/scss/styles.scss';
+import MainLayout from 'layouts/MainLayout';
 
 const MyApp: React.FC<Props> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <MainLayout>
+      <Component {...pageProps} />
+    </MainLayout>
+  );
 };
 
 interface Props {
