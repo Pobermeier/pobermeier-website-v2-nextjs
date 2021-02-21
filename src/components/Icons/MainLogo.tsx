c17ee6213e42517cad86f07a18219680d5c26d18
Patrick Obermeier <patrick.obermeier@outlook.com>
2021-02-21 17:37:15 +0100

    created MainLogoImg-component

---
+++ src/components/Icons/MainLogo.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const MainLogoImg: React.FC<Props> = ({ width, height }) => {
+  return (
+    <img
+      src="img/logo.svg"
+      className="main-logo"
+      width={width}
+      height={height}
+      alt="Patrick Obermeier"
+      title="Back to top"
+    />
+  );
+};
+
+MainLogoImg.defaultProps = {
+  width: 50,
+  height: 50,
+};
+
+interface Props {
+  width?: number;
+  height?: number;
+}
+
+export default MainLogoImg;
