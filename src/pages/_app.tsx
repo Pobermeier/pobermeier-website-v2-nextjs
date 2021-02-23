8d9c07708bca4b5cf21ed0965925956ab103a95a
Patrick Obermeier <patrick.obermeier@outlook.com>
2021-02-23 23:15:33 +0100

    added useEmailPlaceholders-hook such that it runs when app-component has mounted

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import { ComponentClass, useState } from 'react';
 import { AppProps } from 'next/app';
-import 'styles/scss/styles.scss';
 import MainLayout from 'layouts/MainLayout';
+import useEmailPlaceholders from 'hooks/useEmailPlaceholders';
+import 'styles/scss/styles.scss';
 
 const MyApp: React.FC<Props> = ({ Component, pageProps }) => {
   const [isNavMenuOpen, setIsNavMenuOpen] = useState(false);
+
+  useEmailPlaceholders();
 
   return (
     <MainLayout isNavbarOpen={isNavMenuOpen} setIsNavMenuOpen={setIsNavMenuOpen}>
