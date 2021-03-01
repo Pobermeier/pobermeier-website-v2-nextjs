64b2df6a144f6aeb81db22756dba0eaa3bd9e0ea
Patrick Obermeier <patrick.obermeier@outlook.com>
2021-03-01 22:38:04 +0100

    updated global config with new props

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -11,34 +11,30 @@ const SEO: React.FC<Props> = ({ title, description, keywords }) => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>{title}</title>
       <meta name="description" content={description} />
+      <meta name="author" content={config.meta.siteOwnerName} />
       <meta name="keywords" content={keywords} />
       <meta name="theme-color" content={config.meta.themeColor} />
       <link rel="canonical" href={`${config.meta.canonicalRootUrl}${router.asPath}`} />
+      <meta name="msvalidate.01" content={config.meta.bingValidationId} />
 
       {/* <!-- Social Media Stuff / Sharing --> */}
       {/* <!-- Open Graph (Facebook) --> */}
       <meta property="og:url" content={`${config.meta.canonicalRootUrl}${router.asPath}`} />
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
-      <meta
-        property="og:image"
-        content="https://images.ctfassets.net/2hiw8js7vnar/2lp32wEzpzwoRKSEgpj5k4/ab48007f6d160cfc6b26e087206d1840/large_image.png"
-      />
+      <meta property="og:image" content={config.meta.socialShareImgUrl} />
       <meta property="og:description" content={description} />
-      <meta property="og:site_name" content="Patrick Obermeier Portfolio" />
-      <meta property="article:author" content="Patrick Obermeier" />
+      <meta property="og:site_name" content={config.meta.defaultTitle} />
+      <meta property="article:author" content={config.meta.siteOwnerName} />
 
       {/* <!-- Twitter Cards --> */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:url" content={`${config.meta.canonicalRootUrl}${router.asPath}`} />
       <meta name="twitter:title" content={title} />
-      <meta name="twitter:site" content="@Pobermeier87" />
-      <meta name="twitter:creator" content="@Pobermeier87" />
+      <meta name="twitter:site" content={config.meta.twitterHandle} />
+      <meta name="twitter:creator" content={config.meta.twitterHandle} />
       <meta name="twitter:description" content={description} />
-      <meta
-        name="twitter:image"
-        content="https://images.ctfassets.net/2hiw8js7vnar/2lp32wEzpzwoRKSEgpj5k4/ab48007f6d160cfc6b26e087206d1840/large_image.png"
-      ></meta>
+      <meta property="og:image" content={config.meta.socialShareImgUrl} />
     </Head>
   );
 };
--- src/config.ts
+++ src/config.ts
@@ -2,12 +2,17 @@ import projects from 'data/projects';
 
 export default {
   meta: {
+    bingValidationId: 'D5238646980530643F5FC0FFBF63FB97',
     canonicalRootUrl: 'https://www.patrickobermeier.dev',
     defaultTitle: 'Patrick Obermeier | Frontend Developer & Online-Marketing Expert',
     defaultDescription:
       'Patrick Obermeier is an experienced front-end developer and online-marketing expert from Vienna, Austria. Creating web apps and websites using latest technologies like React & the MERN-Stack with passion!',
     defaultKeywords:
       'frontend developer, front-end developer, web developer, webdesign, web designer, html5, css, javascript, react, redux, mern-stack, node, mongodb, express, docker, linux, portfolio, projects, coder, programmer, web apps, websites, marketing sites, landing pages',
+    siteOwnerName: 'Patrick Obermeier',
+    twitterHandle: '@Pobermeier87',
+    socialShareImgUrl:
+      'https://images.ctfassets.net/2hiw8js7vnar/2lp32wEzpzwoRKSEgpj5k4/ab48007f6d160cfc6b26e087206d1840/large_image.png',
     themeColor: '#f5f5f5',
   },
   projects: [...projects],
