87ce48831d574a4b0c1627ab2b23aeb4b042dd1f
Patrick Obermeier <patrick.obermeier@outlook.com>
2021-03-01 14:46:07 +0100

    added useAOS-hook in main app-component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import { ComponentClass, useState } from 'react';
 import { AppProps } from 'next/app';
 import MainLayout from 'layouts/MainLayout';
 import useEmailPlaceholders from 'hooks/useEmailPlaceholders';
+import useAOS from 'hooks/useAOS';
 import 'styles/scss/styles.scss';
 
 const MyApp: React.FC<Props> = ({ Component, pageProps }) => {
   const [isNavMenuOpen, setIsNavMenuOpen] = useState(false);
 
   useEmailPlaceholders();
+  useAOS();
 
   return (
     <MainLayout isNavbarOpen={isNavMenuOpen} setIsNavMenuOpen={setIsNavMenuOpen}>
