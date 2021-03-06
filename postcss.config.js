6c07c7fa89b0deb92212c62fe71dc675e0772388
Patrick Obermeier <patrick.obermeier@outlook.com>
2021-03-06 20:10:08 +0100

    created custom postcss-config

---
+++ postcss.config.js
@@ -0,0 +1,47 @@
+/* eslint-disable */
+module.exports = {
+  plugins: [
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false,
+        },
+      },
+    ],
+    [
+      '@fullhuman/postcss-purgecss',
+      {
+        content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+        safelist: [
+          'html',
+          'body',
+          'fa',
+          'fa-envelope',
+          'is-active',
+          'is-outlined',
+          'enhanced',
+          'lazy-background',
+          'aos-init',
+          'aos-animate',
+          'is-info',
+          'is-primary',
+          'cookie-banner',
+          'cookie-banner__content',
+          'cookie-banner__text',
+          'cookie-banner__buttons',
+          'cookie-banner__accept-button',
+          'cookie-banner__info-button',
+          'hero',
+          'hero-body',
+        ],
+      },
+    ],
+  ],
+};
