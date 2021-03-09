b8f129dd07237b5be65b0a65ecb80f134546c401
Patrick Obermeier <patrick.obermeier@outlook.com>
2021-03-09 20:26:51 +0100

    removed animate.css from project due to performance issues on mobile

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -54,11 +54,6 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
 
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
-          />
-
           <script
             dangerouslySetInnerHTML={{
               __html: `
